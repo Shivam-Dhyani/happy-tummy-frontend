5bcb7b5b2c275e07634e2c841e98f623daf5c0ed
Shivam-Dhyani <shivamdhiyani15@gmail.com>
2024-11-23 23:04:09 +0530

    feat: F-5 Whatsapp Config File Added

---
+++ src/utils/whatsappConfig.js
@@ -0,0 +1,5 @@
+export const handleSendMessage = (message) => {
+  const encodedMessage = encodeURIComponent(message);
+  const whatsappUrl = `https://wa.me/?text=${encodedMessage}`;
+  window.open(whatsappUrl, "_blank");
+};
